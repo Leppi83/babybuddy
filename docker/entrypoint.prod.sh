ad4dd11284492861b14036f645f14ab7eb22bcd2
Leppi83 <151251812+Leppi83@users.noreply.github.com>
2026-03-04 16:20:53 +0100

    Change Gunicorn bind port from 8000 to 8015

--- docker/entrypoint.prod.sh
+++ docker/entrypoint.prod.sh
@@ -7,7 +7,7 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 exec gunicorn babybuddy.wsgi:application \
-  --bind 0.0.0.0:8000 \
+  --bind 0.0.0.0:8015 \
   --workers "${GUNICORN_WORKERS:-3}" \
   --timeout "${GUNICORN_TIMEOUT:-60}" \
   --access-logfile -
